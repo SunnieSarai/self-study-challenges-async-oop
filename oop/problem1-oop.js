eab7aa32cebf541c9dc6202b055b5bad0af97652
Xavier Roberson <xavierroberson177@gmail.com>
2025-09-26 17:36:10 -0400

    OOP completed

--- oop/problem1-oop.js
+++ oop/problem1-oop.js
@@ -1,8 +1,9 @@
 /**
  * CHALLENGE 1: Bird
  * You are working with National Geographic on a bird tracking application.
- * You are tasked with creating a function that returns objects representing
- * different birds. Each `Bird` object should contain the following properties,
+ * You are tasked with
+ *  /creating a function that returns objects representing different birds./ 
+ * Each `Bird` object should contain the following properties,
  * which are initially set via arguments when the object is created:
  *
  * `species` (string representing the bird's species)
@@ -11,6 +12,7 @@
  *
  * The following methods must also be available to every instance of a Bird object
  * (but not stored on the individual objects themselves):
+ * 
  * `getColor` (returns the color of the bird)
  * `setColor` (sets a new color for the bird)
  * `getLocations` (returns the locations where the bird was found)
@@ -18,6 +20,33 @@
  *
  * The methods that take an argument must accept only one argument, and it must be a string.
  */
+class Bird {
+    constructor(species, color, locations){
+    this.species = species;
+    this.color = color;
+    this.locations = [locations];
+    }
+
+   getColor(){
+    return this.color;
+   }
+  
+   setColor(color){
+    this.color = color;
+   }
+   getLocations() {
+    return this.locations;
+   }
+   addLocation(loc){
+    this.locations.push(loc);
+   }
+}
+
+
+
+
+
+
 
 // UNCOMMENT THESE LINES TO CHECK YOUR WORK
 // const newBird = new Bird('canary', 'red', ['newyork', 'spain']);
